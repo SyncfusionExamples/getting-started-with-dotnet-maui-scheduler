ae025d53a21c7171c9512273b0f413b33dc2b841
Ammu1492 <60737792+Ammu1492@users.noreply.github.com>
2022-12-08 22:28:28 +0530

    Added a sample

---
+++ Model/ControlModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulerSample
+{
+    public class ControlModel
+    {
+        public DateTime EventStart { get; set; }
+        public DateTime EventEnd { get; set; }
+        public string Name { get; set; }
+        public Brush Color { get; set; }
+    }
+}
